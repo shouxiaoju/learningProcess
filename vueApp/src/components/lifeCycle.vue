17cef8a780731a40f163649e7e13767001ca57fe
shouxiaoju <shouxiaoju@163.com>
2023-11-21 13:32:41 +0800

    生命周期

--- vueApp/src/components/lifeCycle.vue
+++ vueApp/src/components/lifeCycle.vue
@@ -2,6 +2,9 @@
     <div id='lifeCycle'>
         <p>{{cont}}</p>
         <el-button @click="cont++">点击+1</el-button>
+        <h4>生命周期就是用来描述一个组件从引入到退出的全过程。 那复杂来说呢？就是一个组件从创建开始经历了数据初始化，挂载，更新等步骤后，最后被销毁。</h4>
+        
+        <p></p>
     </div>
 </template>
 
