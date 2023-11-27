ef916c04ee6eea754536d28df2d50df3db89c53f
shouxiaoju <shouxiaoju@163.com>
2023-11-27 19:00:58 +0800

    生命周期

--- vueApp/src/components/lifeCycle.vue
+++ vueApp/src/components/lifeCycle.vue
@@ -3,19 +3,37 @@
         <p>{{cont}}</p>
         <el-button @click="cont++">点击+1</el-button>
         <h4>生命周期就是用来描述一个组件从引入到退出的全过程。 那复杂来说呢？就是一个组件从创建开始经历了数据初始化，挂载，更新等步骤后，最后被销毁。</h4>
-        
-        <p></p>
+        <h4>组件创建阶段</h4>
+        <p>
+            beforeCreate 创建前<br>
+            created 创建后，在Vue实例创建完毕状态，我们可以去访问data、computed、watch、methods上的方法和数据，但现在还没有将虚拟Dom挂载到真实Dom上，所以我们在此时访问不到我们的Dom元素（el属性，ref属性此时都为空）。<br>
+            beforeMount 载入前，它是在挂载之前被调用的，会在此时去找到虚拟Dom，并将其编译成Render<br>
+            mounted 载入后，虚拟Dom已经被挂载到真实Dom上，此时我们可以获取Dom节点，$ref在此时也是可以访问的。<br>
+        </p>
+        <h4>组件更新阶段</h4>
+        <p>
+            beforeUpdate 更新前，响应式数据更新的时候会被调用，beforeupdate的阶段虚拟Dom还没更新，所以在此时依旧可以访问现有的Dom。我们可以在此时访问现有的Dom，手动移除一些添加的监听事件<br>
+            updated 更新后，此时补丁已经打完了，Dom已经更新完毕，可以执行一些依赖新Dom的操作。但还是不建议在此时进行数据操作，避免进入死循环<br>
+        </p>
+        <h4>组件卸载阶段</h4>
+        <p>
+            beforeDestroy 卸载前， 在Vue实例销毁之前被调用，在此时我们的实例还未被销毁。在此时可以做一些操作，比如销毁定时器，解绑全局事件，销毁插件对象等<br>
+            destroyed 卸载后<br>
+        </p>
     </div>
 </template>
 
 <script>
-
+import VuePrismEditor from './vuePrismEditor.vue';
 export default {
    name:"LifeCycle",
+   components:{
+    VuePrismEditor
+   },
    data(){
        return{
-           cont:0
-       }
+           cont:0,
+            }
    },
    beforeCreate(){
        console.log("beforeCreate");
---
+++ vueApp/src/components/vuePrismEditor.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="vuePrismEditor">
+    <prism-editor
+      class="my-editor"
+      :value="values"
+      :highlight="highlighter"
+      readonly
+      line-numbers
+    ></prism-editor>
+  </div>
+</template>
+
+<script>
+// import Prism Editor
+import { PrismEditor } from "vue-prism-editor";
+import "vue-prism-editor/dist/prismeditor.min.css";
+
+// import highlighting library
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism-tomorrow.css";
+export default {
+  name: "VuePrismEditor",
+  props: {
+    values: String
+  },
+  components: {
+    PrismEditor
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    highlighter(code) {
+      return highlight(code, languages.js);
+    }
+  }
+};
+</script>
+<style scoped>
+#vuePrismEditor {
+}
+.my-editor {
+  /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
+  background: #2d2d2d;
+  color: #ccc;
+
+  /* you must provide font-family font-size line-height. Example: */
+  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 5px;
+  margin: 10px 0;
+}
+
+/* optional class for removing the outline */
+.prism-editor__textarea:focus {
+  outline: none;
+}
+</style>
